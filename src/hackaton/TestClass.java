be7126819e6ef76e04e375483689609f108b013d
Rox <stan.lidia.roxana@gmail.com>
2015-10-22 08:23:16 +0300

    Changed prj structure

--- src/hackaton/TestClass.java
+++
@@ -1,9 +0,0 @@
-package hackaton;
-
-public class TestClass {
-
-	public static void main(String[] args) {
-		System.out.println("Bla bla bla");
-	}
-
-}
---
+++ src/main/java/hackaton/TestClass.java
@@ -0,0 +1,14 @@
+package hackaton;
+
+public class TestClass {
+
+	private static String roxSays() {
+		return "\r\n At last! Imi merge si mie!";
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Bla bla bla" + roxSays());
+	}
+
+}
